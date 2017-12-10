f1fb797225d316437227c23fb8510d61bd180609
赛冷思 <silence@sailengsi.com>
2017-12-10 10:03:09 +0800

    test listen window

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 console.log('this is sw.js');
 self.addEventListener('fetch', function(event) {
   	console.log(event);
-  	window.localStorage.setItem('cur_page', window.location.href);
+  	console.log(window.localStorage, self.localStorage);
+  	console.log(window.location, self.location);
+  	// window.localStorage.setItem('cur_page', window.location.href);
 });
