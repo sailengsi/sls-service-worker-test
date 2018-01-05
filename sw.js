2606b4c859dba2b3a2c0b89eb1ea5a6de52f922b
赛冷思 <silence@sailengsi.com>
2018-01-05 15:04:59 +0800

    测试alert

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 console.log('this is sw.js');
 self.addEventListener('fetch', function(event) {
-	console.log(event);
-	console.log(self.localStorage);
-	console.log(self.location);
+	console.log(self.alert);
+	// console.log(event);
+	// console.log(self.localStorage);
+	// console.log(self.location);
 	// window.localStorage.setItem('cur_page', window.location.href);
 });
