9a5884df03c2d7c58c7b6e39f39e607877fee2a4
赛冷思 <silence@sailengsi.com>
2018-01-05 15:15:25 +0800

    test fetch

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 console.log('this is sw.js');
 self.addEventListener('fetch', function(event) {
+	fetch('https://cdn.bootcss.com/jquery/3.2.1/core.js');
 	console.log(self.alert);
 	// console.log(event);
 	// console.log(self.localStorage);
