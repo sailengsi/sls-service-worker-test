40d59873b724eb2954ecf0956642b11c32eebe54
赛冷思 <silence@sailengsi.com>
2018-01-05 14:53:08 +0800

    delete global window object

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 console.log('this is sw.js');
 self.addEventListener('fetch', function(event) {
-  	console.log(event);
-  	console.log(window.localStorage, self.localStorage);
-  	console.log(window.location, self.location);
-  	// window.localStorage.setItem('cur_page', window.location.href);
+	console.log(event);
+	console.log(self.localStorage);
+	console.log(self.location);
+	// window.localStorage.setItem('cur_page', window.location.href);
 });
