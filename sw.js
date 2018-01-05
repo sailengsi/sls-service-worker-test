357f6a102844a38aea325ce37dd43d29af25ddd8
赛冷思 <silence@sailengsi.com>
2018-01-05 15:27:11 +0800

    test fetch js and exec 3

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 console.log('this is sw.js');
 self.addEventListener('fetch', function(event) {
-	fetch('/sls-service-worker-test/test.js');
+	fetch('/sls-service-worker-test/test.js').then(res => {
+		console.log(res);
+	});
 	console.log(self.alert);
 	// console.log(event);
 	// console.log(self.localStorage);
